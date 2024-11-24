bb772a540142e0730db72b11b2ebfedd7db2acb1
Melitonas <melitonas@gmail.com>
2024-11-24 11:49:46 +0200

    refactor: Move ElementaryOS color palette to ui package

--- run.sh
+++ run.sh
@@ -9,8 +9,9 @@ tmux send-keys -t gio-lab.0 "tmux clear-history" ENTER
 # tmux send-keys -t gio-lab.0 "go run ./015-key-pointer-events/" ENTER
 # tmux send-keys -t gio-lab.0 "go run ./020-scroll-widgets/" ENTER
 # tmux send-keys -t gio-lab.0 "go run ./030-fuzzy/" ENTER
-tmux send-keys -t gio-lab.0 "go run ./040-modal/" ENTER
+# tmux send-keys -t gio-lab.0 "go run ./040-modal/" ENTER
 # tmux send-keys -t gio-lab.0 "go run ./050-fps/" ENTER
 # tmux send-keys -t gio-lab.0 "go run ./060-drag-hello/" ENTER
+tmux send-keys -t gio-lab.0 "go run ./070-ui/" ENTER
 # tmux send-keys -t gio-lab.0 "go run ./100-draw-bug/" ENTER
-# tmux send-keys -t gio-lab.0 "go run ." ENTER
+tmux send-keys -t gio-lab.0 "go run ." ENTER
