2a888ad17e0579305e26338afb04522028266e24
Melitonas <melitonas@gmail.com>
2024-12-01 21:32:04 +0200

    updated key events example with modal box

--- 015-key-pointer-events/main.go
+++ 015-key-pointer-events/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"image"
 	"image/color"
 	"log"
 	"os"
@@ -16,6 +17,7 @@ import (
 	"gioui.org/op/paint"
 	"gioui.org/text"
 	"gioui.org/unit"
+	"gioui.org/widget"
 	"gioui.org/widget/material"
 	"github.com/olablt/gio-lab/ui"
 	// "gioui.org/widget/material"
@@ -25,6 +27,7 @@ type Area struct {
 	Name         string
 	PointerPress bool
 	KeyPress     bool
+	Key          key.Name
 	// subscribe to key events
 	Keys                   []key.Name
 	areaStack              clip.Stack
@@ -35,12 +38,6 @@ type Area struct {
 func (a *Area) ProcessEvents(gtx layout.Context) {
 	// Declare the tag.
 	tag := a
-
-	// Confine the area of interest to a gtx Max
-	a.areaStack = clip.Rect{Max: gtx.Constraints.Max}.Push(gtx.Ops)
-
-	// new input op
-	event.Op(gtx.Ops, tag)
 
 	// New pointer event reading
 	a.PointerPress = false
@@ -57,7 +54,7 @@ func (a *Area) ProcessEvents(gtx layout.Context) {
 		ev, ok := event.(pointer.Event)
 		if ok {
 			// handle ev
-			log.Printf("got pointer event %#+v", ev)
+			// log.Printf("got pointer event %#+v", ev)
 			switch ev.Kind {
 			case pointer.Press:
 				a.PointerPress = true
@@ -70,6 +67,8 @@ func (a *Area) ProcessEvents(gtx layout.Context) {
 				a.StatusFocused = true
 				// log
 				log.Printf("[%v] got Pointer.Enter", a.Name)
+			// case pointer.Move:
+			// 	log.Printf("[%v] got Pointer.Move %+v", a.Name, ev.Position)
 			case pointer.Leave:
 				// log
 				log.Printf("[%v] got Pointer.Leave", a.Name)
@@ -82,7 +81,6 @@ func (a *Area) ProcessEvents(gtx layout.Context) {
 		}
 	}
 
-	event.Op(gtx.Ops, tag)
 	// New key event reading
 	filters := []event.Filter{}
 	if a.CaptureKeysWhenInFocus {
@@ -98,6 +96,7 @@ func (a *Area) ProcessEvents(gtx layout.Context) {
 	}
 
 	// New key event reading
+	a.Key = ""
 	a.KeyPress = false
 	for {
 		event, ok := gtx.Event(filters...)
@@ -112,44 +111,117 @@ func (a *Area) ProcessEvents(gtx layout.Context) {
 		if ev.State == key.Press {
 			// log.Printf("[%v] got key event %#+v", a.Name, ev)
 			switch ev.Name {
-			case key.NameEscape:
-				// log
-				log.Printf("[%v] got key.Escape", a.Name)
-				a.KeyPress = true
-			case key.NameReturn:
-				// log
-				log.Printf("[%v] got key.Return", a.Name)
-				a.KeyPress = true
+			// case key.NameEscape:
+			// 	// log
+			// 	log.Printf("[%v] got key.Escape", a.Name)
+			// 	a.KeyPress = true
+			// 	a.Key = ev.Name
+			// case key.NameReturn:
+			// 	// log
+			// 	log.Printf("[%v] got key.Return", a.Name)
+			// 	a.KeyPress = true
+			// 	a.Key = ev.Name
 			default:
 				// log
 				log.Printf("[%v] got key.%v", a.Name, ev.Name)
 				a.KeyPress = true
+				a.Key = ev.Name
 			}
 		}
 	}
 
 }
 
+func (a *Area) Layout(gtx layout.Context) {
+	// Confine the area of interest to a gtx Max
+	a.areaStack = clip.Rect{Max: gtx.Constraints.Max}.Push(gtx.Ops)
+	event.Op(gtx.Ops, a)
+}
+
+// stop collecting events for this area
 func (a *Area) Pop() {
 	a.areaStack.Pop()
 }
 
 func main() {
+	clickable := widget.Clickable{}
+	modalVisible := false
+
 	Loop(func(win *app.Window, gtx layout.Context, th *material.Theme) {
-		ChartLayout(gtx, th)
+
+		// process events from previous frame
+		modalArea.ProcessEvents(gtx)
+		chartArea.ProcessEvents(gtx)
+		barsArea.ProcessEvents(gtx)
+		xArea.ProcessEvents(gtx)
+		yArea.ProcessEvents(gtx)
+		if chartArea.Key == "P" {
+			modalVisible = true
+		}
+		if modalArea.Key == key.NameEscape || clickable.Clicked(gtx) {
+			log.Println("GOT ESC MODAL")
+			modalVisible = false
+		}
+
+		layout.Flex{Axis: 1}.Layout(gtx,
+			// HEADER
+			layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+				th.Fg = color.NRGBA{0, 0, 0, 255}
+				return material.Label(th, unit.Sp(30), "Header").Layout(gtx)
+			}),
+			// CHART
+			layout.Flexed(5, func(gtx layout.Context) layout.Dimensions {
+				inset := layout.UniformInset(unit.Dp(0))
+				return inset.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+					return ChartLayout(gtx, th)
+				})
+			}),
+		)
+
+		// MODAL
+		if modalVisible {
+			gtx.Execute(key.FocusCmd{Tag: modalArea})
+
+			layout.Background{}.Layout(gtx,
+				func(gtx layout.Context) layout.Dimensions {
+					// white transparent background
+					return clickable.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+						// log.Printf("%+v", gtx.Constraints)
+						dims := ui.ColorBox(gtx, gtx.Constraints.Max, ui.Alpha(white, 150))
+						return dims
+					})
+				},
+				func(gtx layout.Context) layout.Dimensions {
+					w := gtx.Dp(200)
+					h := gtx.Dp(300)
+					gtx.Constraints.Max = image.Pt(w, h)
+					gtx.Constraints.Min = image.Pt(w, h)
+					modalArea.Layout(gtx)
+					defer modalArea.Pop()
+					dims := ui.FillWithLabelH3(gtx, th, " Modal ", darkenColor(blue, modalArea.StatusFocused))
+					return dims
+				})
+
+		}
+
 	})
+
+	// Loop(func(win *app.Window, gtx layout.Context, th *material.Theme) {
+	// 	ChartLayout(gtx, th)
+	// })
 }
 
 // define event areas
-var windowArea = &Area{Name: "Window", Keys: []key.Name{key.NameEscape, "Q"}, CaptureKeysWhenInFocus: false}
-var chartArea = &Area{Name: "Chart", Keys: []key.Name{key.NameEscape}, CaptureKeysWhenInFocus: true}
+var chartArea = &Area{Name: "Chart", Keys: []key.Name{key.NameEscape, "Q", "P"}, CaptureKeysWhenInFocus: false}
+var barsArea = &Area{Name: "Bars", Keys: []key.Name{key.NameEscape}, CaptureKeysWhenInFocus: true}
 var xArea = &Area{Name: "X", Keys: []key.Name{"1", "2"}, CaptureKeysWhenInFocus: true}
 var yArea = &Area{Name: "Y", Keys: []key.Name{"1", "2"}, CaptureKeysWhenInFocus: true}
+var modalArea = &Area{Name: "Modal", Keys: []key.Name{key.NameEscape, "Q", "1", "2"}, CaptureKeysWhenInFocus: true}
 
 func ChartLayout(gtx layout.Context, th *material.Theme) layout.Dimensions {
 	// whole window events
-	windowArea.ProcessEvents(gtx)
-	defer windowArea.Pop()
+	chartArea.Layout(gtx)
+	defer chartArea.Pop()
 
 	return layout.Flex{}.Layout(gtx,
 		// MAIN CHART & X Axis AREA
@@ -158,13 +230,13 @@ func ChartLayout(gtx layout.Context, th *material.Theme) layout.Dimensions {
 			return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
 				// MAIN CHART AREA
 				layout.Flexed(0.5, func(gtx layout.Context) layout.Dimensions {
-					chartArea.ProcessEvents(gtx)
-					chartArea.Pop()
-					return ui.FillWithLabelH3(gtx, th, "Chart", darkenColor(green, chartArea.StatusFocused))
+					barsArea.Layout(gtx)
+					barsArea.Pop()
+					return ui.FillWithLabelH3(gtx, th, "Bars", darkenColor(green, barsArea.StatusFocused))
 				}),
 				// X Axis AREA
 				layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-					xArea.ProcessEvents(gtx)
+					xArea.Layout(gtx)
 					xArea.Pop()
 					return ui.FillWithLabelH3(gtx, th, "X", darkenColor(red, xArea.StatusFocused))
 				}),
@@ -172,7 +244,7 @@ func ChartLayout(gtx layout.Context, th *material.Theme) layout.Dimensions {
 		}),
 		// Y Axis
 		layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-			yArea.ProcessEvents(gtx)
+			yArea.Layout(gtx)
 			yArea.Pop()
 			return ui.FillWithLabelH3(gtx, th, " Y ", darkenColor(blue, yArea.StatusFocused))
 		}),
@@ -187,6 +259,7 @@ var (
 	background = color.NRGBA{R: 0xC0, G: 0xC0, B: 0xC0, A: 0xFF}
 	alpha      = uint8(255)
 	red        = color.NRGBA{R: 0xC0, G: 0x40, B: 0x40, A: alpha}
+	white      = color.NRGBA{R: 0xff, G: 0xff, B: 0xff, A: alpha}
 	green      = color.NRGBA{R: 0x40, G: 0xC0, B: 0x40, A: alpha}
 	blue       = color.NRGBA{R: 0x40, G: 0x40, B: 0xC0, A: alpha}
 )
@@ -197,12 +270,13 @@ func darkenColor(c color.NRGBA, focused bool) color.NRGBA {
 		return c
 	}
 	// Reduce each color component by ~25% for focused state
-	return color.NRGBA{
-		R: uint8(float64(c.R) * 0.75),
-		G: uint8(float64(c.G) * 0.75),
-		B: uint8(float64(c.B) * 0.75),
-		A: c.A,
-	}
+	return ui.Alpha(c, 200)
+	// return color.NRGBA{
+	// 	R: uint8(float64(c.R) * 0.75),
+	// 	G: uint8(float64(c.G) * 0.75),
+	// 	B: uint8(float64(c.B) * 0.75),
+	// 	A: c.A,
+	// }
 }
 
 func Loop(fn func(win *app.Window, gtx layout.Context, th *material.Theme)) {
--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@ tmux send-keys -t gio-lab.0 "tmux clear-history" ENTER
 # tmux send-keys -t gio-lab.0 "go run ./000-black-on-white/" ENTER
 # tmux send-keys -t gio-lab.0 "go run ./000-hello/" ENTER
 # tmux send-keys -t gio-lab.0 "go run ./010-layout-stacked/" ENTER
-# tmux send-keys -t gio-lab.0 "go run ./015-key-pointer-events/" ENTER
+tmux send-keys -t gio-lab.0 "go run ./015-key-pointer-events/" ENTER
 # tmux send-keys -t gio-lab.0 "go run ./020-scroll-widgets/" ENTER
 # tmux send-keys -t gio-lab.0 "go run ./030-fuzzy/" ENTER
 # tmux send-keys -t gio-lab.0 "go run ./040-modal/" ENTER
 # tmux send-keys -t gio-lab.0 "go run ./050-fps/" ENTER
 # tmux send-keys -t gio-lab.0 "go run ./060-drag-hello/" ENTER
-tmux send-keys -t gio-lab.0 "go run ./070-ui/" ENTER
+# tmux send-keys -t gio-lab.0 "go run ./070-ui/" ENTER
 # tmux send-keys -t gio-lab.0 "go run ./100-draw-bug/" ENTER
